e1d634ba365a4b0a5c6801ee5421525a2677e9ba
terrces <113709421+Terrces@users.noreply.github.com>
2024-10-29 19:39:33 +0500

    test

--- Scripts/Localization/Translater.js
+++ Scripts/Localization/Translater.js
@@ -1,11 +1,12 @@
 let TranslateFetch = await fetch("../../Data/Translate.json");
-let Translate = await TranslateFetch.json()
+let Translate = await TranslateFetch.json();
 
 let Lang = localStorage.getItem("lang");
 function localizator(){
-        document.querySelector("#BuyGifRefresh").innerHTML = '<img class="buttonimg" src="../Pictures/Icons/update.svg" style="height:1em;"> ' + Translate[Lang].main.refresh;
-        document.querySelector("#AppendInCollection").innerHTML =  '<img class="buttonimg" src="../Pictures/Icons/bookmark.svg" style="height:1em;"> ' + Translate[Lang].main.add_gif_to_collection;
-        document.querySelector("#CollectionOpen").innerHTML =  '<img class="buttonimg" src="../Pictures/Icons/collection.svg" style="height:1em;">' + Translate[Lang].main.collection;
-        document.querySelector("#OpenShopWindow").innerHTML =  '<img class="buttonimg" src="../Pictures/Icons/shop.svg" style="height:1.2em; margin-bottom:-3px;">' + Translate[Lang].main.shop;
+        // document.querySelector(".ButtonBack").innerHTML = Translate[lang].general.back;
+        document.querySelector("#BuyGifRefresh").innerHTML ='<img class="buttonimg" src="../Pictures/Icons/update.svg" style="height:1em;">' + Translate[Lang].main.refresh;
+        document.querySelector("#AppendInCollection").innerHTML ='<img class="buttonimg" src="../Pictures/Icons/bookmark.svg" style="height:1em;">' + Translate[Lang].main.add_gif_to_collection;
+        document.querySelector("#CollectionOpen").innerHTML ='<img class="buttonimg" src="../Pictures/Icons/collection.svg" style="height:1em;">' + Translate[Lang].main.collection;
+        document.querySelector("#OpenShopWindow").innerHTML ='<img class="buttonimg" src="../Pictures/Icons/shop.svg" style="height:1.2em; margin-bottom:-3px;">' + Translate[Lang].main.shop;
 }
 localizator();
