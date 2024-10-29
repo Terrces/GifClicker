27ea8e2941c43a0b4b09c2fd29b82973a81041c5
terrces <113709421+Terrces@users.noreply.github.com>
2024-10-29 22:09:25 +0500

    2

--- Scripts/Localization/Translater.js
+++ Scripts/Localization/Translater.js
@@ -4,10 +4,10 @@ let Translate = await TranslateFetch.json();
 let Lang = localStorage.getItem("lang");
 function localizator(){
         // document.querySelector(".ButtonBack").innerHTML = Translate[lang].general.back;
-        document.querySelector("#BuyGifRefresh").innerHTML ='<img class="buttonimg" src="../Pictures/Icons/update.svg" style="height:1em;">' + "<text>" + Translate[Lang].main.refresh + "</text>";
+        console.log(Translate[Lang].main.refresh);
+        document.querySelector("#BuyGifRefresh").innerHTML ='<img class="buttonimg" src="../Pictures/Icons/update.svg" style="height:1em;">' + Translate[Lang].main.refresh;
         document.querySelector("#AppendInCollection").innerHTML ='<img class="buttonimg" src="../Pictures/Icons/bookmark.svg" style="height:1em;">';
-        document.querySelector("#CollectionOpen").innerHTML ='<img class="buttonimg" src="../Pictures/Icons/collection.svg" style="height:1em;">' + "<text>" + Translate[Lang].main.collection  + "</text>";
-        document.querySelector("#OpenShopWindow").innerHTML ='<img class="buttonimg" src="../Pictures/Icons/shop.svg" style="height:1.2em; margin-bottom:-3px;">' + "<text>" + Translate[Lang].main.shop + "</text>";
-
+        document.querySelector("#CollectionOpen").innerHTML ='<img class="buttonimg" src="../Pictures/Icons/collection.svg" style="height:1em;">' + Translate[Lang].main.collection;
+        document.querySelector("#OpenShopWindow").innerHTML ='<img class="buttonimg" src="../Pictures/Icons/shop.svg" style="height:1.2em; margin-bottom:-3px;">' + Translate[Lang].main.shop;
 }
 localizator();
