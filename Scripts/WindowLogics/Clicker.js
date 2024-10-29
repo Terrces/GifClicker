90ed749eae90deaa413301cdfa0375190974e867
terrces <113709421+Terrces@users.noreply.github.com>
2024-10-29 08:42:01 +0500

    New settings menu, and new backgrounds

--- Scripts/WindowLogics/Clicker.js
+++ Scripts/WindowLogics/Clicker.js
@@ -30,13 +30,6 @@ if(localStorage.getItem('GifCollection') != null){
 	GifLibrary = JSON.parse(localStorage.getItem('GifCollection'));
 }
 
-document.body.style.backgroundImage = "url(https://images.unsplash.com/photo-1700909415807-9abe5be462cb?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)";
-document.body.style.backgroundPositionY = 50 + "%";
-document.body.style.backgroundPositionX = 50 + "%";
-document.body.style.backgroundRepeat = "no-repeat";
-document.body.style.backgroundSize = "cover";
-
-
 export function upgrade (id,addmultiply,addprice) {
 	if(GifCoin >= priceUpgrades[id]){
 		StatsCountUpgrades[id] += 1;
