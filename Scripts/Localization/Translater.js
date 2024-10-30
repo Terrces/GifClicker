4d31ba71174a1d79b5d482dac949c49d396a5886
terrces <113709421+Terrces@users.noreply.github.com>
2024-10-30 22:34:24 +0300

    Test localization path №2

--- Scripts/Localization/Translater.js
+++ Scripts/Localization/Translater.js
@@ -1,4 +1,4 @@
-let TranslateFetch = await fetch("../../../Data/Translate.json");
+let TranslateFetch = await fetch("../Data/Translate.json");
 let Translate = await TranslateFetch.json();
 
 let Lang = localStorage.getItem("lang");
