e0562b795b9c3eb1db7eb33ff9f3480e9fbe7dbf
terrces <113709421+Terrces@users.noreply.github.com>
2024-10-31 03:51:30 +0500

    full adapted for phones

--- Scripts/Style.js
+++ Scripts/Style.js
@@ -1,1 +1,16 @@
 document.body.style.opacity = "100%";
+
+window.addEventListener('resize',() => {
+    let windowOuterWidth = window.outerWidth;
+    try
+    {
+        if(windowOuterWidth <= 650){
+            document.getElementById("count").style.marginTop = "64px";
+        }
+        else
+        {
+            document.getElementById("count").style.marginTop = "15px";
+        }
+        
+    }catch{}
+})
